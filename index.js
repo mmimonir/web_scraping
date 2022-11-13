e152155dd51d9eac1c38ee0e67b6446ba3b1b1c8
Md. Monirul Islam <mmimonir.ctg@gmail.com>
2022-11-14 00:10:04 +0600

    some testing

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log(12);
+const puppeteer = require("puppeteer");
+
+async function run() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://bajajpoint.blogspot.com");
+  //   await page.screenshot({ path: "example.png", fullPage: true });
+
+  const links = await page.evaluate(() =>
+    Array.from(document.querySelectorAll("a"), (a) => a.href)
+  );
+
+  console.log(links);
+
+  await browser.close();
+}
+run();
